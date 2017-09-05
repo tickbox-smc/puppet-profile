a28265e3b474ff19db5efa95e43439572654d1c5
root <root@puppet.tickbox>
2017-09-05 21:04:46 +0000

    test pip install

--- manifests/python.pp
+++ manifests/python.pp
@@ -7,5 +7,12 @@ class profile::python {
     gunicorn   => 'absent',
   }
 
-  ::python::requirements { '/vagrant/requirements.txt': }
+  #::python::requirements { '/vagrant/requirements.txt': }
+
+  ::python::pip { 'Flask':
+    pkgname => 'Flask',
+    ensure  => '0.10.1',
+    timeout => 1800,
+  }
+
 }
