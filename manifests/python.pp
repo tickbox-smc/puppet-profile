20aa5e64c99fb5ba1873572621e071c30f5bbf8c
root <root@puppet.tickbox>
2017-09-05 18:28:47 +0000

    simplify python.pp for bugfix

--- manifests/python.pp
+++ manifests/python.pp
@@ -1,11 +1,3 @@
 class profile::python {
-  class { '::python':
-    version    => 'python34',
-    pip        => 'present',
-    dev        => 'absent',
-    virtualenv => 'absent',
-    gunicorn   => 'absent',
-  }
+  include ::python
 }
-
-
